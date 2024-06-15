c84adf6fbef0fee66d507291b38a3a22d77b0880
Greg Thomas Andes <andesgregthomas@gmail.com>
2024-06-15 17:55:14 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const {isAddress} = require("ethers");
+const { isAddress } = require("ethers");
 const Distribute = require("./distribute");
 const Balance = require("./balance");
-const {json, urlencoded} = require("express");
+const { json, urlencoded } = require("express");
 
 /**
  * Initialize Express Application
@@ -21,27 +21,30 @@ app.get("/", (_, res) => {
 	return res.status(200).send("API Distributor Healthy");
 });
 
-app.get("/claim/:address", async(req, res) => {
-
+app.get("/claim/:address", async (req, res) => {
 	const { address } = req.params;
 
 	if (!isAddress(address)) return res.status(400).send("Invalid Ethereum Address");
 
-	const distribute = await Distribute({ address });
-
-	return res.status(200).send({
-		distribute
-	});
-
-
+	try {
+		const distribute = await Distribute({ address });
+		return res.status(200).send({ distribute });
+	} catch (error) {
+		return res.status(500).send("Claim transaction failed");
+	}
 });
 
-app.get("/balance", async(_, res) => {
-	return res.status(200).send({
-		balance: await Balance()
-	});
-});
 
+app.get("/balance", async (_, res) => {
+	try {
+		const balance = await Balance();
+		return res.status(200).json({
+			balance
+		});
+	} catch (error) {
+		return res.status(500).send("Error obtaining balance");
+	}
+});
 
 app.listen(8888, () => {
 	console.log("SKALE API Distributor Listening on ", 8888);
